a88f8f20e4cc5f9552955cce23bd9a5a63f622f2
Soraya Benson <soraya.benson@gmail.com>
2021-06-10 15:02:00 -0700

    a functional nav bar! woohoooo!

--- src/components/UI/NavBar.jsx
+++ src/components/UI/NavBar.jsx
@@ -1,45 +1,12 @@
-// import React from "react";
-// import Button from "@material-ui/core/Button";
-// import Menu from "@material-ui/core/Menu";
-// import AppBar from '@material-ui/core/AppBar';
-// import MenuItem from "@material-ui/core/MenuItem";
-
-// const NavBar=() => {
-//   const [anchorEl, setAnchorEl] = React.useState(null);
-//   const handleClick = event => {
-//     setAnchorEl(event.currentTarget);
-//   };
-//   const handleClose = () => {
-//     setAnchorEl(null);
-//   };
-//   return (
-//     <AppBar position="static">
-//       <Button onClick={handleClick}>Open</Button>
-//       <Menu
-//         anchorEl={anchorEl}
-//         keepMounted
-//         open={Boolean(anchorEl)}
-//         onClose={handleClose}
-//       >
-//         <MenuItem onClick={handleClose}>home</MenuItem>
-//         <MenuItem onClick={handleClose}>songbook</MenuItem>
-//         <MenuItem onClick={handleClose}>partyroom</MenuItem>
-//         <MenuItem onClick={handleClose}>abount</MenuItem>
-//     </AppBar>
-//   );
-// };
-
-
 import React, { useState } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import Button from '@material-ui/core/Button';
+import { AppBar, Menu, MenuItem, Button} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import { Link } from 'react-router-dom';
 
 
 const Navbar = () => { 
   const [anchorEl, setAnchorEl] = useState(null);
+
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
   };
@@ -57,17 +24,29 @@ const Navbar = () => {
         keepMounted
         open={Boolean(anchorEl)}
         onClose={handleClose}>
-        <MenuItem onClick={handleClose}>
-              Home
+        <MenuItem 
+          component={Link}
+          to='/'
+          onClick={handleClose}>
+              home
         </MenuItem>
-        <MenuItem onClick={handleClose}>
-              Songbook
+        <MenuItem 
+          component={Link}
+          to='/greenroom'
+          onClick={handleClose}>
+              green room
         </MenuItem>
-        <MenuItem onClick={handleClose}>
-              Party Room
+        <MenuItem 
+          component={Link}
+          to='/partyroom'
+          onClick={handleClose}>
+              party room
         </MenuItem>
-        <MenuItem onClick={handleClose}>
-              Abount
+        <MenuItem 
+          component={Link}
+          to='/about'
+          onClick={handleClose}>
+              about
         </MenuItem>
               
       </Menu>
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import '../../components/styles/app.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from '../../containers/Home';
+import GreenRoom from '../../containers/GreenRoom';
+import PartyRoom from '../../containers/PartyRoom';
+import Abount from '../../containers/Abount';
+import '../styles/app.css';
 import NavBar from '../UI/NavBar';
 
 export default function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-    </>
+      <Switch>
+        <Route path="/" exact render={(routerProps) => <Home {...routerProps} />} />
+        <Route path="/greenroom" render={(routerProps) => <GreenRoom {...routerProps} />}/>
+        <Route path="/partyroom" render={(routerProps) => <PartyRoom {...routerProps} />}/>
+        <Route path="/about" render={(routerProps) => <Abount {...routerProps} />}/>
+      </Switch>
+    </Router>
   );
 }
 

21a59ee25d0acb829c8ba16324cb9c05d3a1a687
Nicole Martin <n.martinpdx@gmail.com>
2021-06-11 16:40:36 -0700

    add Video embedd to Party Room, basic styling in Party Room

--- src/containers/PartyRoom.jsx
+++ src/containers/PartyRoom.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Video from '../components/Video/Video';
+import Chat from '../components/chat/Chat';
+import { Grid, Typography } from '@material-ui/core';
 
 const PartyRoom = () => {
+
+    const chatMessages = [
+        {
+            stageName: 'soraya',
+            message: 'hi',
+            timeStamp: 'noon'
+        },
+        {
+            stageName: 'nicole',
+            message: 'hi',
+            timeStamp: 'noon.1'
+        },
+        {
+            stageName: 'soraya',
+            message: 'AHH',
+            timeStamp: 'noon.2'
+        },
+    ];
+
+    const [roomName, setRoomName] = useState('Frosty Darkness');
+    // const [messages, setMessages] = useState('');
+    // const [queue, setQueue] = useState([]);
+
+
+
     return (
         <div>
-            party room
+            <Grid
+                container
+                justify="center"
+                alignItems="center"
+                direction="column"
+                style={{ minHeight: '100vh' }}
+                spacing={5}>
+                <Typography>
+                    Room Name: {roomName}
+                </Typography>
+                <Grid item style={{ border: '1px solid #000' }}>
+                    <Video embedId={ 'CgHNvCUSSvg'} />
+                </Grid>
+                <Grid item style={{ border: '1px solid #000' }}>
+                    <Chat chatMsgs={chatMessages} />
+                </Grid>
+
+            </Grid>
         </div>
-    )
-}
+    );
+};
 
 export default PartyRoom;
+
+
+

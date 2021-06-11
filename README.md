cbc85c1361bae1d39968a2586fe46bdcda158c31
Brandon Perard <brandon.perard@gmail>
2021-06-11 16:55:52 -0700

    add socket.io skeletion to Chat component

--- README.md
+++ README.md
@@ -2,5 +2,8 @@ Libraries installed:
 (1) Material Ui
     Material Ui/Icons
     Material Ui/System
-
+    Socket.IO(BE)
+    SOcket.IO-client(FE)
+    Express
+    
     
--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import ChatMsg from './ChatMsg';
+import { Socket } from "socket.io-client";
 
-const Chat = ({ chatMsgs }) => (
-    <ul aria-label='chat'>
-        {chatMsgs.map((msg) => (
-            <li key={msg.message}>
-                <ChatMsg {...ChatMsg} />
-            </li>
-        ))}
-    </ul>
-);
+const Chat = () => {
+    const [newMessage, setNewMessage] = useState('');
+    const [messageArray, setMessageArray] = useState([]);
+
+    const sendMessage = () => {
+        socket.emit('SEND_MESSAGE', newMessage);
+
+        setNewMessage('');
+    };
+
+    useEffect(() => {
+        socket.on('SEND_MESSAGE', (newMessage) => {
+            setMessageArray(...messageArray, newMessage);
+        });
+    }, []);
+
+
+
+    return (
+        <>
+            <ul aria-label='chat'>
+                {messageArray.map((msg) => (
+                    <li key={msg.message}>
+                        <ChatMsg {...msg} />
+                    </li>
+                ))}
+            </ul>
+            <input type="text" value={newMessage} onChange={({ target }) => setNewMessage(target.value)} />
+            <button onClick={sendMessage}>Send Message</button>
+        </>
+    );
+};
 
 Chat.propTypes = {
-    chatMsgs: PropTypes.arrayOf(
+    messageArray: PropTypes.arrayOf(
         PropTypes.shape({
             stageName: PropTypes.string.isRequired,
             message: PropTypes.string.isRequired,

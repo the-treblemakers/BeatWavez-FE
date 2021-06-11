26ae98617b143d38b8ffa26e78f7c9bef79db5a7
Lori <74270598+LoriWinston@users.noreply.github.com>
2021-06-11 15:28:18 -0700

    Dev lori (#5)
    * initial material ui install and icons
    
    * building search form
    
    * starting to build out green room

--- src/components/UI/NavBar.jsx
+++ src/components/UI/NavBar.jsx
@@ -6,76 +6,76 @@ import MenuIcon from '@material-ui/icons/Menu';
 import { useStyles } from '../styles/styles';
 
 const Navbar = (props) => { 
-  const [anchorEl, setAnchorEl] = useState(null);
-  const { pathname } = props.location;
+    const [anchorEl, setAnchorEl] = useState(null);
+    const { pathname } = props.location;
 
-  const handleClick = (e) => {
-    setAnchorEl(e.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const handleClick = (e) => {
+        setAnchorEl(e.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <AppBar
-      className={classes.appBar}
-      position="static">
-      <Grid container direction="row" justify="space-between" alignItems="center">
-        <Button 
-          aria-haspopup="true"
-          aria-owns={anchorEl ? 'menu' : null}
-          onClick={handleClick}>
-          <MenuIcon/>
-        </Button>
-        <Menu 
-          id="menu"
-          className={classes.menu}
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}>
-          <MenuItem 
-            className={classes.link}
-            component={Link}
-            to='/'
-            onClick={handleClose}
-            selected={pathname === '/'}
-            >
+    return (
+        <AppBar
+            className={classes.appBar}
+            position="static">
+            <Grid container direction="row" justify="space-between" alignItems="center">
+                <Button 
+                    aria-haspopup="true"
+                    aria-owns={anchorEl ? 'menu' : null}
+                    onClick={handleClick}>
+                    <MenuIcon/>
+                </Button>
+                <Menu 
+                    id="menu"
+                    className={classes.menu}
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}>
+                    <MenuItem 
+                        className={classes.link}
+                        component={Link}
+                        to='/'
+                        onClick={handleClose}
+                        selected={pathname === '/'}
+                    >
                 home
-          </MenuItem>
-          <MenuItem 
-            component={Link}
-            to='/greenroom'
-            onClick={handleClose}
-            selected={pathname === '/greenroom'}
-            >
+                    </MenuItem>
+                    <MenuItem 
+                        component={Link}
+                        to='/greenroom'
+                        onClick={handleClose}
+                        selected={pathname === '/greenroom'}
+                    >
                 green room
-          </MenuItem>
-          <MenuItem 
-            component={Link}
-            to='/partyroom'
-            onClick={handleClose}
-            selected={pathname === '/partyroom'}
-            >
+                    </MenuItem>
+                    <MenuItem 
+                        component={Link}
+                        to='/partyroom'
+                        onClick={handleClose}
+                        selected={pathname === '/partyroom'}
+                    >
                 party room
-          </MenuItem>
-          <MenuItem 
-            component={Link}
-            to='/about'
-            onClick={handleClose}
-            selected={pathname === '/about'}
-            >
+                    </MenuItem>
+                    <MenuItem 
+                        component={Link}
+                        to='/about'
+                        onClick={handleClose}
+                        selected={pathname === '/about'}
+                    >
                 about
-          </MenuItem>   
-        </Menu>
+                    </MenuItem>   
+                </Menu>
 
-        <Typography variant="h1">beatWaves🌴</Typography>
+                <Typography variant="h1">beatWaves🌴</Typography>
         
-      </Grid>
-    </AppBar>
-  );
+            </Grid>
+        </AppBar>
+    );
 }; 
 
 export default withRouter(Navbar);
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from '../UI/NavBar';
 import Home from '../../containers/Home';
 import GreenRoom from '../../containers/GreenRoom';
@@ -10,17 +10,17 @@ import { ThemeProvider } from '@material-ui/core';
 import { theme } from '../styles/theme';
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route path="/" exact render={(routerProps) => <Home {...routerProps} />} />
-        <Route path="/greenroom" render={(routerProps) => <GreenRoom {...routerProps} />}/>
-        <Route path="/partyroom" render={(routerProps) => <PartyRoom {...routerProps} />}/>
-        <Route path="/about" render={(routerProps) => <Abount {...routerProps} />}/>
-      </Switch>
-    </Router>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Router>
+                <NavBar />
+                <Switch>
+                    <Route path="/" exact render={(routerProps) => <Home {...routerProps} />} />
+                    <Route path="/greenroom" render={(routerProps) => <GreenRoom {...routerProps} />}/>
+                    <Route path="/partyroom" render={(routerProps) => <PartyRoom {...routerProps} />}/>
+                    <Route path="/about" render={(routerProps) => <Abount {...routerProps} />}/>
+                </Switch>
+            </Router>
+        </ThemeProvider>
+    );
 }

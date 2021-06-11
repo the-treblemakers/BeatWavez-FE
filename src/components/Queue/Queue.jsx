eb641c5da3707a38459e4484fc8907bf574676f8
KB (Katy Boyles) <74560886+katrinkajb@users.noreply.github.com>
2021-06-10 17:29:58 -0700

    Dev kb (#3)
    * added queueItem and queue components
    
    * fixed typo
    
    * stubbed out chat components

---
+++ src/components/Queue/Queue.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import QueueItem from "./QueueItem";
+
+const Queue = ({ queueItems }) => (
+    <ul aria-label="queue">
+        {queueItems.map((item) => (
+            <li key={item.title}>
+                <QueueItem {...QueueItem} />
+            </li>
+        ))}
+    </ul>
+);
+
+Queue.propTypes = {
+    queueItems: PropTypes.arrayOf(
+        PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            stageName: PropTypes.string.isRequired,
+            thumbnail: PropTypes.string.isRequired,
+        })
+    ),
+};
+
+export default Queue;
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from '../../containers/Home';
-import GreenRoom from '../../containers/GreenRoom';
-import PartyRoom from '../../containers/PartyRoom';
-import Abount from '../../containers/Abount';
-import '../styles/app.css';
-import NavBar from '../UI/NavBar';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../../containers/Home";
+import GreenRoom from "../../containers/GreenRoom";
+import PartyRoom from "../../containers/PartyRoom";
+import Abount from "../../containers/Abount";
+import "../styles/app.css";
+import NavBar from "../UI/NavBar";
 
 export default function App() {
-  return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route path="/" exact render={(routerProps) => <Home {...routerProps} />} />
-        <Route path="/greenroom" render={(routerProps) => <GreenRoom {...routerProps} />}/>
-        <Route path="/partyroom" render={(routerProps) => <PartyRoom {...routerProps} />}/>
-        <Route path="/about" render={(routerProps) => <Abount {...routerProps} />}/>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <NavBar />
+            <Switch>
+                <Route
+                    path="/"
+                    exact
+                    render={(routerProps) => <Home {...routerProps} />}
+                />
+                <Route
+                    path="/greenroom"
+                    render={(routerProps) => <GreenRoom {...routerProps} />}
+                />
+                <Route
+                    path="/partyroom"
+                    render={(routerProps) => <PartyRoom {...routerProps} />}
+                />
+                <Route
+                    path="/about"
+                    render={(routerProps) => <Abount {...routerProps} />}
+                />
+            </Switch>
+        </Router>
+    );
 }
 
 // import React, { useState } from 'react';
@@ -29,13 +42,12 @@ export default function App() {
 // import Button from '@material-ui/core/Button';
 // import MenuIcon from '@material-ui/icons/Menu';
 
-
 // const Navbar = () => {
 //   const [open, setOpen] = useState(false);
 
 //   return (
 //     <AppBar position="static">
-//       <Menu 
+//       <Menu
 //         // anchorEl={anchorEl}
 //         open={open}>
 //         <MenuIcon/>
@@ -51,7 +63,7 @@ export default function App() {
 //         <MenuItem>
 //               Abount
 //         </MenuItem>
-              
+
 //       </Menu>
 //     </AppBar>
 //   );

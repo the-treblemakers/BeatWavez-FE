928a1e0f1ecc9f989470af75842b739e51b5444f
Nicole Martin <n.martinpdx@gmail.com>
2021-06-11 13:29:03 -0700

    add home page function and disable buttons

--- src/components/UI/LoginForm.jsx
+++ src/components/UI/LoginForm.jsx
@@ -1,36 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Button, Grid } from '@material-ui/core';
+import InstructionAlert from '../UI/InstructionAlert';
 
 const LoginForm = () => {
+  const [stageName, setStageName] = useState('');
+  const [roomName, setRoomName] = useState('');
+  // const [newRoomCode, setNewRoomCode] = useState('');
+
+  const handleStageNameChange = (e) => {
+    setStageName(e.target.value);
+  };
+
+  const handleRoomNameChange = (e) => {
+    setRoomName(e.target.value);
+  };
+
   return (
-    <Grid container direction="column" alignItems="center" justify="center">
-      <TextField
-        variant="outlined"
-        label="Stage Name"
-        fullWidth
-        style={{ marginBottom: '1em' }}
-      />
+    <>
+      <Grid container direction="column" alignItems="center" justify="center">
+        <TextField
+          variant="outlined"
+          label="Stage Name"
+          fullWidth
+          style={{ marginBottom: '1em' }}
+          onChange={handleStageNameChange}
+          value={stageName}
+        />
 
-      <TextField
-        variant="outlined"
-        label="Room Name"
-        fullWidth
-        style={{ marginBottom: '1em' }}
-      />
+        <TextField
+          variant="outlined"
+          label="Room Name"
+          fullWidth
+          style={{ marginBottom: '1em' }}
+          onChange={handleRoomNameChange}
+          value={roomName}
+        />
 
-      <Button
-        size="large"
-        variant="contained"
-        color="primary"
-        style={{ marginBottom: '2em' }}
-      >
-        JOIN PARTY!
-      </Button>
+        <Button
+          size="large"
+          variant="contained"
+          color="primary"
+          style={{ marginBottom: '2em' }}
+          disabled={!stageName || !roomName}
+          // onClick={() => setDisabled(true)}
+        >
+          JOIN THE PARTY!
+        </Button>
 
-      <Button size="large" variant="contained" color="primary">
-        GET NEW ROOM
-      </Button>
-    </Grid>
+        <Button
+          size="large"
+          variant="contained"
+          color="primary"
+          disabled={!stageName}
+          // value={newRoomCode}
+          // onClick={}
+        >
+          GET NEW ROOM CODE
+        </Button>
+        {/* <p>Here is your room code: {newRoomCode}</p> */}
+      </Grid>
+      <InstructionAlert />
+    </>
   );
 };
 

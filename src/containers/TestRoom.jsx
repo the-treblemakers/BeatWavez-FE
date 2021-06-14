1f99fd16ab79750c214749285df2c46447fd6778
Brandon Perard <brandon.perard@gmail>
2021-06-14 16:22:22 -0700

    pre-merge commit

--- src/containers/TestRoom.jsx
+++ src/containers/TestRoom.jsx
@@ -1,44 +1,45 @@
 import { SettingsInputSvideoRounded } from "@material-ui/icons";
 import React, { useState, useEffect } from "react";
 import ChatList from "../components/chat/ChatList";
-// import io from "socket.io-client";
+import io from "socket.io-client";
 
-// const socket = io('http://localhost:4001/');
+// const socket = io('http://localhost:7890');
+const socket = io('https://beatwavez-dev.herokuapp.com/');
 
 // pass in name prop to attach to message
 // can add timestamp with new Date() at time in message handler
 //
 const TestRoom = () => {
-    // const [newMessage, setNewMessage] = useState('');
-    // const [stageName, setStageName] = useState('');
-    // const [messageArray, setMessageArray] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
+    const [stageName, setStageName] = useState('');
+    const [messageArray, setMessageArray] = useState([]);
 
-    // useEffect(() => {
-    //     socket.on('MESSAGE', (message) => {
-    //         setMessageArray([...messageArray, message]);
-    //     });
+    useEffect(() => {
+        socket.on('MESSAGE', (message) => {
+            setMessageArray([...messageArray, message]);
+        });
 
-    //     return () => {
-    //         socket.disconnect();
-    //     };
-    // }, [newMessage, stageName, messageArray]);
+    }, [newMessage, stageName, messageArray]);
 
-    // const handleNewMessage = () => {
-    //     socket.emit('MESSAGE', { newMessage, stageName, timeStamp: 'TIME' });
-    //     setNewMessage('');
-    // };
+    const handleNewMessage = () => {
+        console.log('CLICK HANDLE');
+        socket.emit('MESSAGE', { message: newMessage, stageName, timeStamp: new Date() });
+        setNewMessage('');
+    };
 
     return (
         <div>
             <h1>TESTING</h1>
-            {/* <input type="text" value={stageName} placeholder="Enter Stage Name" onChange={({ target }) => setStageName(target.value)} />
+            <input type="text" value={stageName} placeholder="Enter Stage Name" onChange={({ target }) => setStageName(target.value)} />
             <ChatList messageArray={messageArray} />
             <input type="text" value={newMessage} onChange={({ target }) => setNewMessage(target.value)} />
-            <button onClick={handleNewMessage}>Send Message</button> */}
+            <button onClick={handleNewMessage}>Send Message</button>
         </div>
 
     );
 };
+
+fullArray.slice(a, b);
 
 export default TestRoom;
 

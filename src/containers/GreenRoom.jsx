96318fa4a7f8ad3a77125b59ae14950418d609fb
Lori Winston <Lcwinston@gmail.com>
2021-06-14 15:42:58 -0700

    got our chat working through heroku!

--- src/containers/GreenRoom.jsx
+++ src/containers/GreenRoom.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import SearchBar from '../components/Search/SearchBar';
 import SearchFilters from '../components/Search/SearchFilters';
-import Songbook from '../components/SongBook/Songbook';
+// import Songbook from '../components/SongBook/Songbook';
 // import Queue from '../components/Queue/Queue';
-// import Chat from '../components/Chat/Chat';
+import Chat from '../components/Chat/Chat';
 
 const GreenRoom = () => {
     
@@ -13,9 +13,9 @@ const GreenRoom = () => {
             GreenRoom
             <SearchBar />
             <SearchFilters />
-            <Songbook />
-            {/*<Queue />
-            <Chat /> */}
+            {/* <Songbook /> */}
+            {/*<Queue /> */}
+            <Chat /> 
         </div>
     );
 };

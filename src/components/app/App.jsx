c20e988dd44cd698e0a8c40026c698464e8899a6
KB (Katy Boyles) <74560886+katrinkajb@users.noreply.github.com>
2021-06-14 14:11:32 -0700

    Dev kb (#12)
    * added queueItem and queue components
    
    * fixed typo
    
    * stubbed out chat components
    
    * little tweaks to songbook and queue items
    
    * got songbook loading in greenroom
    
    * updated key for song list

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,14 +4,13 @@ import NavBar from '../UI/NavBar';
 import Home from '../../containers/Home';
 import GreenRoom from '../../containers/GreenRoom';
 import PartyRoom from '../../containers/PartyRoom';
-import Abount from '../../containers/About';
+import About from '../../containers/About';
 import '../styles/app.css';
 import { ThemeProvider } from '@material-ui/core';
 import { theme } from '../styles/theme';
 import TestRoom from '../../containers/TestRoom';
 
 export default function App() {
-
     return (
         <ThemeProvider theme={theme}>
             <Router>
@@ -20,11 +19,10 @@ export default function App() {
                     <Route path="/" exact render={(routerProps) => <Home {...routerProps} />} />
                     <Route path="/greenroom" render={(routerProps) => <GreenRoom {...routerProps} />} />
                     <Route path="/partyroom" render={(routerProps) => <PartyRoom {...routerProps} />} />
-                    <Route path="/about" render={(routerProps) => <Abount {...routerProps} />} />
+                    <Route path="/about" render={(routerProps) => <About {...routerProps} />} />
                     <Route path="/test" render={(routerProps) => <TestRoom {...routerProps} />} />
                 </Switch>
             </Router>
         </ThemeProvider>
     );
-
 }

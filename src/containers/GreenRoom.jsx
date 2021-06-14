d125ed04b786c43c388d4aa90e8afb0778bee5d2
KB <katrinkajb@gmail.com>
2021-06-14 13:33:41 -0700

    got songbook loading in greenroom

--- src/containers/GreenRoom.jsx
+++ src/containers/GreenRoom.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import SearchBar from '../components/Search/SearchBar';
-// import Songbook from '../components/SongBook/Songbook';
+import Songbook from '../components/SongBook/Songbook';
 // import Queue from '../components/Queue/Queue';
 // import Chat from '../components/Chat/Chat';
 
 const GreenRoom = () => {
+    
+    
     return (
         <div>
             GreenRoom
             <SearchBar />
-            {/* <Songbook />
-            <Queue />
+            <Songbook />
+            {/*<Queue />
             <Chat /> */}
         </div>
     );

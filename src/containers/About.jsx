33cd7ce4221b14d0149104513e7b668fbedf25fb
Lori Winston <Lcwinston@gmail.com>
2021-06-15 15:05:19 -0700

    added profile pics

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,54 +1,69 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 
 const About = () => {
     return (
         <div>
-            <h1>Meet the KJs</h1>
-            <p>
-                <h2>KB</h2>
+            <Grid>
+                <h1>Meet the KJs</h1>
+                <p>
+                    <h2>KB</h2>
+                    <img src=""/>
+                    KB is cool af and wonderful all around.
                 LinkedIn: https://www.linkedin.com/in/katy-boyles/
 
                 Github: https://github.com/katrinkajb
 
                 Twitter: https://twitter.com/KBtrizay
                 
-            </p>
-            <p>
-                <h2>NICOLE</h2>
+                </p>
+                <p>
+                    <h2>NICOLE</h2>
+                    <img src=""/>
+                    Nicole is amazing and the best pup parent.
                 LinkedIn: https://www.linkedin.com/in/nicolemartinpdx/
 
                 Github: https://github.com/nicole-m-martin
 
                 Twitter: https://twitter.com/nmartinpdx
                 
-            </p>
-            <p>
-                <h2>BRANDON</h2>
+                </p>
+                <p>
+                    <h2>BRANDON</h2>
+                    <img src=""/>
+                    Brandon is a badass karaoke-afficianado.
                 LinkedIn: https://www.linkedin.com/in/brandonperard/
 
                 Github: https://github.com/bperard
 
                 Twitter: https://twitter.com/SayYesToExcess
                 
-            </p>
-            <p>
-                <h2>SORAYA</h2>
+                </p>
+                <p>
+                    <h2>SORAYA</h2>
+                    <img src=""/>
+                    Soraya is a perfect talented angel.
                 LinkedIn: https://www.linkedin.com/in/soraya-benson/
 
                 Github: https://github.com/sorayabenson
 
                 Twitter: https://twitter.com/sorayamajd
                 
-            </p>
-            <p>
-                <h2>LORI</h2>
+                </p>
+                <p>
+                    <h2>LORI</h2>
+                    <img src=""/>
+                Lori is a software engineer who loves hiking, cooking, and petting dogs. 
+                She has a soft spot for Electric Light Orchestra at karaoke despite Jeff
+                Lynne's angelic voice being far out of her vocal range. 
                 LinkedIn: https://www.linkedin.com/in/loriwinston/
 
                 Github: https://github.com/LoriWinston
 
                 Twitter: https://twitter.com/LoriWinston8
                 
-            </p>
+                </p>
+            </Grid>
         </div>
     );
 };

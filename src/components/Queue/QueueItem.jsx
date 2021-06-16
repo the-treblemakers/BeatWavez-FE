e47a25c925cfef103c06b515056b40d2d8218ff6
Soraya Benson <74790499+sorayabenson@users.noreply.github.com>
2021-06-15 18:56:37 -0700

    Dev soraya (#22)
    * initial material ui install and icons
    
    * working hamburger menu!!!
    
    * a functional nav bar! woohoooo!
    
    * nav grid updated
    
    * nav menu highlights current page
    
    * navbar updated themes and styles
    
    * menu dropdown refiend and styleable
    
    * video card component and controls started
    
    * video controls added
    
    * next and previous buttons functional for queue controls
    
    * play key in progress
    
    * spinner created and gorgeous
    
    * merge
    
    * add to queue is functional
    
    * queue added to partyroom
    
    * file cleaned up
    
    Co-authored-by: Lori Winston <Lcwinston@gmail.com>

--- src/components/Queue/QueueItem.jsx
+++ src/components/Queue/QueueItem.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const QueueItem = ({ title, stageName, thumbnail }) => (
+const QueueItem = ({ title, stageName }) => (
     <>
-        <img src={thumbnail} alt={title} />
-        <span>Singer: {stageName}</span>
-        <span>Song: {title}</span>
-        {/* <span>{channelName}</span> */}
+        <p>{stageName}: {title}</p>
     </>
 );
 
 QueueItem.propTypes = {
     title: PropTypes.string.isRequired,
     stageName: PropTypes.string.isRequired,
-    thumbnail: PropTypes.string.isRequired,
-    // channelName: PropTypes.string.isRequired,
 };
 
 export default QueueItem;

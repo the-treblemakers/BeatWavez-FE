7ce2aec9b020fc7a5698dbb04d9466f216e45956
KB <katrinkajb@gmail.com>
2021-06-17 10:28:16 -0700

    updates from Dan help

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import generateRoomNames from '../RoomNames/generateRoomNames.js';
 import io from "socket.io-client";
 
-// const socket = io('http://localhost:7890/');
-const socket = io('https://beatwavez-dev.herokuapp.com/');
+const socket = io('http://localhost:7890/');
+// const socket = io('https://beatwavez-dev.herokuapp.com/');
 
 export const useSocket = () => {
     const [newMessage, setNewMessage] = useState('');
@@ -19,38 +19,40 @@ export const useSocket = () => {
 
         socket.on('SET_HOST', ({ hostId }) => {
             setHost({ hostId, isHost: true });
+            console.log('set host', hostId);
         });
 
+        // After joining the room, this goes to room
         socket.on('REQUEST_HOST', ({ sender }) => {
             if(host.isHost) {
                 socket.emit('SEND_HOST', ({ hostId: host.hostId, queue, sender }));
+                console.log('request host', host);
             }
         });
 
         socket.on('RECEIVE_HOST_QUEUE', ({ hostId, queue }) => {
             setHost({ hostId, isHost: false });
             setQueue(queue);
+            console.log('receive queue host', hostId);
         });
 
         socket.on('ADD_TO_QUEUE', ({ title, vidId, stageName, thumbnail }) => {
             setQueue([...queue, { title, vidId, stageName, thumbnail }]);
+            console.log('update queue');
             socket.emit('UPDATE_QUEUE', { queue, roomName: roomInfo.roomName });
         });
 
         socket.on('UPDATE_QUEUE', ({ queue }) => {
             setQueue(queue);
         });
-    }, [roomInfo, messageArray, host, queue]);
-
-    // decide if host can be used outside of hook (maybe to determine song/queue controls)
-    // check room availability logic and create_room insert/add delete on host disconnnect
-
-    useEffect(() => {
         return () => {
             console.log('disconnect fire');
             socket.disconnect();
         };
     }, []);
+
+    // decide if host can be used outside of hook (maybe to determine song/queue controls)
+    // check room availability logic and create_room insert/add delete on host disconnnect
 
     const handleCreateRoom = (stageName) => {
         if(stageName !== '') {
@@ -77,13 +79,14 @@ export const useSocket = () => {
     };
 
     const handleAddToQueue = (song) => {
-        console.log(song);
+        console.log('host', host);
         if(!host.isHost) {
             console.log(roomInfo.stageName, 'REQ ADD');
             socket.emit('ADD_TO_QUEUE', ({ title: song.title, vidId: song.vidId, stageName: roomInfo.stageName, thumbnail: song.thumbnail, hostId: host.hostId }));
         } else if(host.isHost) {
             console.log(roomInfo.stageName, 'HOST ADD');
             setQueue([...queue, { title: song.title, vidId: song.vidId, stageName: roomInfo.stageName, thumbnail: song.thumbnail, hostId: host.hostId }]);
+            socket.emit('UPDATE_QUEUE', { queue, roomName: roomInfo.roomName });
         }
 
     };

cff8f5c3aa769eb4cd61a2417e09749401cf1fef
Soraya Benson <soraya.benson@gmail.com>
2021-06-18 04:14:37 -0700

    alert added back in and refactored into own file

--- src/containers/GreenRoom.jsx
+++ src/containers/GreenRoom.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Accordion, AccordionSummary, AccordionDetails, Typography, Grid, Snackbar, IconButton } from '@material-ui/core';
-import CloseSharpIcon from '@material-ui/icons/CloseSharp';
+import { Accordion, AccordionSummary, AccordionDetails, Typography, Grid } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import SearchBar from '../components/Search/SearchBar';
 import SearchFilters from '../components/Search/SearchFilters';
 import Songbook from '../components/SongBook/Songbook';
 import Queue from '../components/Queue/Queue';
 import Chat from '../components/Chat/Chat';
+import SnackboxAlert from '../components/UI/SnackboxAlert';
 import { getAllSongs } from '../services/apiUtils';
 import Spinner from '../components/UI/Spinner';
 import { useStyles } from '../components/Styles/greenroomStyles';
@@ -20,8 +20,7 @@ const GreenRoom = ({ handleNewMessage, roomInfo, newMessage, messageArray, setNe
     const [currentSongs, setCurrentSongs] = useState([]);
     const [channelFilter, setChannelFilter] = useState('');
     const [filteredSongs, setFilteredSongs] = useState([]);
-    const [open, setOpen] = useState(true);
-    
+        
     const classes = useStyles();
 
     useEffect(() => {
@@ -78,10 +77,6 @@ const GreenRoom = ({ handleNewMessage, roomInfo, newMessage, messageArray, setNe
 
     }
 
-    function handleClose() {
-        setOpen(false);
-    }
-
     return (
         <Grid container
             justify="flex-end"
@@ -106,45 +101,7 @@ const GreenRoom = ({ handleNewMessage, roomInfo, newMessage, messageArray, setNe
                 </Typography>
             }
 
-            {/* <Snackbar
-                open={open}
-                className={classes.snackbar}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center',
-                }}
-                // message={
-                //     <>
-                //         <Typography
-                //             variant="body1"
-                //             color="secondary"
-                //             style={{ margin: "0.7rem" }}>
-                //             Hey {roomInfo.stageName}!
-                //         </Typography>
-                //         <Typography
-                //             variant="body2"
-                //             style={{ margin: "0.7rem" }}>
-                //             To find your song, you can flip through songbook or search your heart out and press + to add it to the queue.
-                //         </Typography>
-                //         <Typography
-                //             style={{ margin: "0.7rem" }}
-                //             variant="body2">
-                //             Hangout here and chat with your party while the host plays the karaoke queue on a shared screen.
-                //         </Typography>
-                //         <Typography
-                //             style={{ margin: "0.7rem" }}
-                //             variant="body2">
-                //             If you're the host, head over to the partyroom to share the karaoke magic with everyone!
-                //         </Typography>
-                //     </>
-                // }
-                autoHideDuration={15000}
-                onClose={handleClose}
-                action={
-                    <IconButton onClick={handleClose}>
-                        <CloseSharpIcon color="primary" />
-                    </IconButton>
-                } /> */}
+            <SnackboxAlert roomInfo={roomInfo} />
 
             <Accordion
                 className={classes.accordion}
